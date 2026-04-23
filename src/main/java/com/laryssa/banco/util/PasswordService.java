fda7e1bd7fac29f6ceddd1b99e75856008798912
Laryssa Martins da Silva <215954902+krystall8778@users.noreply.github.com>
2026-04-23 06:37:40 -0300

    fix : ajustes no código

--- src/main/java/com/laryssa/banco/util/PasswordService.java
+++ src/main/java/com/laryssa/banco/util/PasswordService.java
@@ -9,8 +9,8 @@ public class PasswordService {
             throw new IllegalArgumentException("ERRO: valor inválido");
         }
 
-        String hash = BCrypt.hashpw(senha, BCrypt.gensalt(10));
-        return hash;
+        return BCrypt.hashpw(senha, BCrypt.gensalt(10));
+
     }
 
 
